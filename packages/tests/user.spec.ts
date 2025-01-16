e3dfcfbc7c423fd782a0459d4f9cc22aea60bef0
harkirat <harkirat96@gmail.com>
2025-01-16 21:48:22 +0530

    Added test

---
+++ packages/tests/user.spec.ts
@@ -0,0 +1,33 @@
+import { describe, expect, test, it } from 'vitest'
+import axios from "axios"
+
+const BACKEND_URL = "http://localhost:8080"
+
+const PHONE_NUMBER_1 = "7060334001";
+const NAME_1 = "harkirat";
+
+describe("Signup endpoints", () => {
+
+  it('Double signup doesnt work', async () => {
+    const response1 = await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+      number: PHONE_NUMBER_1,
+    });
+
+    const response2 = await axios.post(`${BACKEND_URL}/api/v1/signup/verify`, {
+      name: NAME_1,
+      otp: "000000"
+    });
+
+    expect(response1.status).toBe(200);
+    expect(response2.status).toBe(200);
+    expect(response1.data.id).not.toBeNull();
+
+    expect(async () => {
+      await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+        number: PHONE_NUMBER_1,
+      });
+    }).toThrow();
+
+  })
+
+})
